71a2d293c70eebe1ad7c42418e41c72ba461d941
Charles Gruenwald <charles@thinktopic.com>
2016-10-26 16:48:53 -0600

    [core] Latest updates.

--- src/wfc/core.clj
+++ src/wfc/core.clj
@@ -30,9 +30,17 @@
               (vec)))
        (frequencies)))
 
-(defn entropy [patch [mr mg mb]]
-  (let [num-pixels (* N N)]
-    (->> patch
+(defn patch->entropy [patch hist]
+  (let [num-pixels (* N N)
+        entropy (->> hist
+                     (map (fn [[pxs wt]]
+                            (reduce (fn [eax [a b]]
+                                      (if (= a b)
+                                        (inc eax)
+                                        eax)) 0 (map (fn [x y] [x y]) pxs patch))))
+                     (apply max))]
+    (double (/ entropy num-pixels))
+    #_(->> patch
          (reduce (fn [[x y z] [r g b]]
                    [(+ (Math/abs (- r mr)) x)
                     (+ (Math/abs (- g mg)) y)
@@ -40,18 +48,33 @@
          ((fn [[a b c]]
             (+ a b c))))))
 
-(defn generate-patch [patch hist]
+(defn filter-hist
+  [patch hist]
   (->> hist
-       (into [])
-       shuffle
-       ffirst))
+       (filter (fn [[pxs wt]]
+                 (every? (fn [[px patch-px]]
+                           (or (= [158 211 172] patch-px)
+                               (= px patch-px)))
+                         (map (fn [x y] [x y]) pxs patch))))
+       (into {})))
+
+(defn patch->new-patch [patch hist]
+    (let [hist (filter-hist patch hist)
+          total (apply + (vals hist))
+          n (rand-int total)]
+      (loop [candidate (first hist)
+             r (rest hist)
+             so-far (second candidate)]
+        (if (or (nil? so-far) (< n so-far))
+          (first candidate)
+          (recur (first r) (rest r) (+ so-far (second (first r))))))))
 
 (defn -main
   [& args]
   (let [src (-> input-image
                 (io/resource)
                 (i/load-image))
-        dest (i/new-image 256 128)
+        dest (i/new-image 64 64)
         hist (img->hist src)
         [mr mg mb] (img->mean-color src)
         entropy-map* (atom {})]
@@ -67,33 +90,48 @@
                                     (i/get-pixels)
                                     (map colour/components-rgb)
                                     (vec))]
-                     [[x y] [(entropy patch [mr mg mb]) patch]]))
+                     [[x y] [(patch->entropy patch hist) patch]]))
                  (into {})))
 
-    (loop [i 1000]
+    (loop [i 2000]
       ;; observe
-      (let [[[xd yd] [_ patch]] (->> @entropy-map*
+      (let [[[xd yd] [chosen-entropy patch]] (->> @entropy-map*
                                      (vec)
                                      (shuffle)
-                                     (sort-by (comp first second) <)
-                                     (first))
-            new-patch (generate-patch patch hist)]
+                                     (apply max-key #(first (second %))))
+            new-patch (patch->new-patch patch hist)]
+
+
 
         ;(if (zero? (mod i 10))
         ;  (println i))
 
-        (println i)
+        (println i (first patch) xd yd (count @entropy-map*) chosen-entropy)
 
         ;; propagate
         (doseq [x (range N)
                 y (range N)]
-          (let [[nr ng nb]  (get new-patch (+ x (* y N)))]
-            (i/set-pixel dest (+ x xd) (+ y yd)
-                         (colour/rgb-from-components nr ng nb))))
+          (let [[nr ng nb]  (get new-patch (+ x (* y N)) [mr mg mb])]
+            (when (and (<= 0 (+ x xd) (- (i/width dest) N))
+                     (<= 0 (+ y yd) (- (i/height dest) N)))
+              (i/set-pixel dest (+ x xd) (+ y yd)
+                           (colour/rgb-from-components nr ng nb)))))
 
-        (swap! entropy-map* assoc [xd yd] [(entropy new-patch [mr mg mb]) new-patch])
+        (doseq [xe (range (- xd (dec N)) (+ xd N))
+                ye (range (- yd (dec N)) (+ yd N))]
+            (if (and (<= 0 xe (- (i/width dest) N))
+                     (<= 0 ye (- (i/height dest) N)))
+              (let [patch (->> (i/sub-image dest xe ye N N)
+                               (i/get-pixels)
+                               (map colour/components-rgb)
+                               (vec))
+                    new-entropy (patch->entropy patch hist)]
+                (if (= new-entropy 1.0)
+                  (swap! entropy-map* dissoc [xe ye])
+                  (if (get @entropy-map* [xe ye])
+                    (swap! entropy-map* assoc [xe ye] [new-entropy patch]))))))
 
-        (if (pos? i)
+        (if (and (pos? i) (pos? (count @entropy-map*)))
           (recur (dec i)))))
     (i/save dest "out.png")
     (i/show dest)))
